f5a0d8c64922426712b93a00c3c3d1f3b405cff8
Marcos Hernandez <maalhequi95@gmail.com>
2025-02-23 08:21:09 -0500

    enable services route

--- src/app.ts
+++ src/app.ts
@@ -91,6 +91,11 @@ class APIGateway {
       this.proxy.web(req, res, { target: `${configuration.get("ORDERS_MICROSERVICE")}/orders` });
     });
 
+    // enable schedule microservice
+    this.app.use("/api/v1/services", (req: Request, res: Response) => {
+      this.proxy.web(req, res, { target: `${configuration.get("ORDERS_MICROSERVICE")}/services` });
+    });
+
     // Start the server
     this.app.listen(this.port, () => {
       console.log(`API Gateway listening on port ${this.port}`);
